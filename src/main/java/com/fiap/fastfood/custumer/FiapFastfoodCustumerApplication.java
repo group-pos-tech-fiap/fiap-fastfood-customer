1aaa93ba34fcc73e4c90aacb58ffeed9fd886978
Diego Silveira <diego_silveira@sicredi.com.br>
2024-09-08 20:10:39 -0300

    initial commit

---
+++ src/main/java/com/fiap/fastfood/custumer/FiapFastfoodCustumerApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.fastfood.custumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapFastfoodCustumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiapFastfoodCustumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiap/fastfood/custumer/FiapFastfoodCustumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.fastfood.custumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapFastfoodCustumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
